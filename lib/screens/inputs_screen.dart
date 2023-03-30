8922bcb9a40bdacd99e6e775ee6db8a3fd8c3c7d
Luis Alberto Virgen Villegas <luisalberto1922@gmail.com>
2023-03-30 10:51:24 -0600

    Se ingresaron campos de texto

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -7,6 +7,16 @@ class InputScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues={
+      'first_name': 'Luis',
+      'last_name': 'Virgen',
+      'email': 'luisalberto1922@gmail.com',
+      'password': '12345',
+      'role': 'admin'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y forms'),
@@ -14,10 +24,22 @@ class InputScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal:20, vertical: 10),
-          child: Column(
-            children: [
-              CustomInputField()
-            ],
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                CustomInputField(
+                  labelText: 'Nombre',
+                  hintText: 'Nombre del usuario',
+                  helperText: 'Solo letras',
+                  counterText: '3 caracteres',
+                  prefixIcon: Icons.group_outlined,
+                  suffixIcon: Icons.assignment_ind_outlined,
+                  icon: Icons.verified_user_outlined,
+                  formProperty: 'first_name', formValues: formValues
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -15,7 +15,12 @@ class CustomInputField extends StatelessWidget {
   final Map<String, String> formValues;
 
   const CustomInputField({
-    super.key, this.hintText, this.labelText, this.helperText, this.counterText, this.prefixIcon, this.suffixIcon, this.icon, this.keyboardType, required this.obscureText, required this.formProperty, required this.formValues,
+    super.key, this.hintText, 
+    this.labelText, this.helperText, 
+    this.counterText, this.prefixIcon, 
+    this.suffixIcon, this.icon, 
+    this.keyboardType, this.obscureText=false, 
+    required this.formProperty, required this.formValues,
   });
 
   @override
@@ -24,33 +29,24 @@ class CustomInputField extends StatelessWidget {
       autofocus: true,
       initialValue: '',
       textCapitalization: TextCapitalization.words,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
       onChanged: (value) {
-        
+        formValues[formProperty]=value;
       },
       validator: (value) {
         if(value==null) return 'Este campo es requerido';
         return value.length < 3 ? 'Minimo de 3 letras': null;
       },
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      decoration: const InputDecoration(
+      decoration: InputDecoration(
         hintText: 'Nombre del usuario',
         labelText: 'Nombre',
         helperText: 'Solo letras',
         counterText: '3 caracteres',
-        prefixIcon: Icon(Icons.group_outlined),
-        suffixIcon: Icon(Icons.assignment_ind_outlined),
-        icon: Icon(Icons.verified_user_outlined),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.lightGreen
-          ),
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(10),
-            topRight: Radius.circular(10)
-          )
-        )
+        prefixIcon: prefixIcon == null ? null : Icon(prefixIcon),
+        suffixIcon: suffixIcon == null ? null : Icon(suffixIcon),
+        icon: Icon == null ? null : Icon(icon)
       ),
     );
   }
